2c7f0a1425e03e7a402126780bff2d5695892886
yendlessse <yoohanse@naver.com>
2025-11-26 17:02:39 +0900

    feat: 로그아웃 구현

--- Linkee-front/Linkee-front-vue/src/main.js
+++ Linkee-front/Linkee-front-vue/src/main.js
@@ -9,7 +9,7 @@ import { createPinia } from 'pinia';
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.use(ElementPlus)
 app.use(router);
 app.mount('#app');
-app.use(pinia);
